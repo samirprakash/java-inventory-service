25b5b85bd45de37a50af40e6845a682e7ee73ba2
Samir Prakash <Samir.Prakash@gmail.com>
2023-03-26 23:18:56 -0500

    fixed skuCode JPA error

--- src/main/java/com/sp/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/sp/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,12 @@
 package com.sp.inventoryservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sp.inventoryservice.model.Inventory;
+import com.sp.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -9,5 +14,21 @@ public class InventoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
+	
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
 
+		return (args) -> {
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("123");
+			inventory1.setQuantity(10);
+			
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("456");
+			inventory2.setQuantity(20);
+			
+			inventoryRepository.save(inventory1);
+			inventoryRepository.save(inventory2);
+		};
+	}
 }
