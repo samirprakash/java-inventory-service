3db71c435abd3e738faa77fae400c59f3ca1b076
Samir Prakash <Samir.Prakash@gmail.com>
2023-04-08 21:36:11 -0500

    increased default database seed to 20

--- src/main/java/com/sp/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/sp/inventoryservice/InventoryServiceApplication.java
@@ -14,20 +14,20 @@ public class InventoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
-	
+
 	@Bean
-	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
 
 		// Create some sample data
 		return (args) -> {
 			Inventory inventory1 = new Inventory();
 			inventory1.setSkuCode("iPhone_14");
 			inventory1.setQuantity(100);
-			
+
 			Inventory inventory2 = new Inventory();
 			inventory2.setSkuCode("iPhone_15");
-			inventory2.setQuantity(0);
-			
+			inventory2.setQuantity(20);
+
 			inventoryRepository.save(inventory1);
 			inventoryRepository.save(inventory2);
 		};
