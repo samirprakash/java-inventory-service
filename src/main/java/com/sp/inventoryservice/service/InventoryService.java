2df7a9345e348d9482ac59f02364a4c1815b0277
Samir Prakash <Samir.Prakash@gmail.com>
2023-04-08 21:56:21 -0500

    simulate timelimiter and retry

--- src/main/java/com/sp/inventoryservice/service/InventoryService.java
+++ src/main/java/com/sp/inventoryservice/service/InventoryService.java
@@ -9,17 +9,32 @@ import com.sp.inventoryservice.dto.InventoryResponse;
 import com.sp.inventoryservice.repository.InventoryRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
-    
+
     private final InventoryRepository inventoryRepository;
-    
+
     @Transactional(readOnly = true)
     public List<InventoryResponse> isInStock(List<String> skuCode) {
-        return inventoryRepository.findBySkuCodeIn(skuCode).stream().map(inventory -> 
-            InventoryResponse.builder().skuCode(inventory.getSkuCode()).isInStock(inventory.getQuantity() > 0).build()
-        ).toList();
+        // Uncomment to Simulate a long running process
+        // This is to demonstrate the TimeLimiter feature
+        log.info("Wait Started");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("Wait Ended");
+
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory -> InventoryResponse.builder()
+                        .skuCode(inventory.getSkuCode())
+                        .isInStock(inventory.getQuantity() > 0)
+                        .build())
+                .toList();
     }
 }
